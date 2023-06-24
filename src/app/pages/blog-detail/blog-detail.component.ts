ea05d376670a2cb12b8352accc137d76cb9174ea
Keval <kevalvadhiya101@gmail.com>
2023-06-24 17:56:21 +0530

    added meta service

--- src/app/pages/blog-detail/blog-detail.component.ts
+++ src/app/pages/blog-detail/blog-detail.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BlogPost } from 'src/app/shared/interfaces/blog-post.interface';
 import { DataSharingService } from 'src/app/shared/service/data-sharing.service';
+import { MetaTagsService } from 'src/app/shared/service/meta-tags.service';
 
 @Component({
   selector: 'app-blog-detail',
@@ -10,14 +11,26 @@ import { DataSharingService } from 'src/app/shared/service/data-sharing.service'
 })
 export class BlogDetailComponent {
 
-  blog: BlogPost;
-  constructor(private dataService: DataSharingService, private route: ActivatedRoute) {}
+  public blog: BlogPost;
+
+  constructor(
+    private dataService: DataSharingService, 
+    private route: ActivatedRoute,
+    private metaService: MetaTagsService) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {   
       this.dataService.getProject(params['id']).subscribe(blog => {
         console.log(blog)
         this.blog = blog;
+
+        const meta = {
+          title: this.blog?.title,
+          description: this.blog?.intro,
+          keywords: '',
+        }
+        const thumbnail = `${window.location.origin}/${this.blog?.thumbnail}`
+        this.metaService.updateMetaTags(meta, thumbnail)
       })
     })
     }
---
+++ src/app/shared/service/meta-tags.service.ts
@@ -0,0 +1,56 @@
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
+export interface IMetaTags {
+  description: string,
+  keywords: string,
+  title: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MetaTagsService {
+  private isBrowser = false;
+  private pageUrl: string;
+
+  constructor(
+    private _meta: Meta, 
+    private title: Title,
+    @Inject(PLATFORM_ID) private platformId: any,
+    @Inject(DOCUMENT) private dom) { }
+
+  createCanonicalLink(url?: string) {
+    if(this.isBrowser) {
+      let canURL = url == undefined ? this.dom.URL : url;
+      this.pageUrl = canURL;
+      let link: HTMLLinkElement = this.dom.createElement('link');
+      link.setAttribute('rel', 'canonical');
+      this.dom.head.appendChild(link);
+      link.setAttribute('href', canURL);
+    }
+  }
+
+  updateMetaTags(meta: IMetaTags, image: string): void {
+    this.title.setTitle(meta?.title);
+
+    this._meta.updateTag( { name: "description", content: meta.description }, "name='description'");
+    this._meta.updateTag( { name: "keywords", content: meta.keywords }, "name='keywords'");
+    this._meta.updateTag( { name: "image", content: image }, "name='image'");
+
+    this._meta.updateTag( { property: 'og:type', content: 'website' }, "property='og:type'");
+    // this._meta.updateTag( { property: 'og:url', content: this.pageUrl }, "property='og:url'");
+    this._meta.updateTag( { property: 'og:title', content: meta.title }, "property='og:title'");
+    this._meta.updateTag( { property: 'og:description', content: meta.description }, "property='og:description'");
+    this._meta.updateTag( { property: 'og:image:secure_url', content: image }, "property='og:image:secure_url'");
+
+    
+    this._meta.updateTag({ name: 'twitter:creator', content: `@KevalVadhiya`}, "name='twitter:creator'")
+    this._meta.updateTag({ name: 'twitter:site', content: `@QuickBlogs`}, "name='twitter:site'")
+    this._meta.updateTag({ name: 'twitter:title', content: meta.title}, "name='twitter:title'")
+    this._meta.updateTag({ name: 'twitter:description', content: meta.description}, "name='twitter:description'")
+    this._meta.updateTag({ name: 'twitter:image', content: `https://bhagavad-gita.netlify.app/assets/krishna-arjuna.jpg`}, "name='twitter:image'")
+    this._meta.updateTag({ name: 'twitter:card', content: 'summary_large_image'}, "name='twitter:card'")
+  }
+}
