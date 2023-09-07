f83888869dabee98f8c7622ce08a13f5f683a6de
Keval <kevalvadhiya101@gmail.com>
2023-09-07 22:16:04 +0530

    Fixes

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -11,7 +11,7 @@ export class ContactComponent {
 
   ngOnInit(): void {
     const meta = {
-      title: '"Contact Us | Quick Blogs - Get in Touch with Our Team',
+      title: 'Contact Us | Quick Blogs - Get in Touch with Our Team',
       description: 'Have questions or feedback? Contact Quick Blogs today! Our dedicated team is here to assist you. Reach out for inquiries, collaborations, or support',
       keywords: 'Contact Us, Quick Blogs, Get in Touch, Contact Information, Customer Support, Inquiry, Collaboration, Feedback, Reach Out, Contact Quick Blogs',
     }
